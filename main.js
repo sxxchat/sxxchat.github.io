93752fd306bf204ce374b9524fc2c85be3b354c7
insk <yasinskiy.vlad@gmail.com>
2017-03-28 12:54:04 +0300

    qq

--- main.js
+++ main.js
@@ -1,65 +1,81 @@
+ //
+ //
+ // var sm = {
+ //   title : "СЯКИ МАКИ",
+ //   price : 2.5,
+ //   weight : 41,
+ //   quantity : 8,
+ //   printOut : function () {
+ //      title = this.title;
+ //      price = this.price + " BYN.";
+ //      weight = this.weight + " г.";
+ //      quantity = this.quantity + " штук";
+ //      console.log(title, price, weight, quantity )
+ //    };
+ // };
+ //
+ // var um = {
+ //   title : "УНАГИ МАКИ",
+ //   price : 1.5,
+ //   weight : 42,
+ //   quantity : 8,
+ //   printOut : function () {
+ //      title = this.title;
+ //      price = this.price + " BYN.";
+ //      weight = this.weight + " г.";
+ //      quantity = this.quantity + " штук";
+ //      console.log(title, price, weight, quantity )
+ //    };
+ // };
+ //
+ // var km = {
+ //   title : "КАППА МАКИ",
+ //   price : 0.5,
+ //   weight : 41,
+ //   quantity : 8,
+ //   printOut : function () {
+ //      title = this.title;
+ //      price = this.price + " BYN.";
+ //      weight = this.weight + " г.";
+ //      quantity = this.quantity + " штук";
+ //      console.log(title, price, weight, quantity )
+ //    };
+ // };
+ //
+ // var ssm = {
+ //   title : "СПАЙСИ СЯКЕ МАКИ",
+ //   price : 2.5,
+ //   weight : 41,
+ //   quantity : 8,
+ //   printOut : function () {
+ //      title = this.title;
+ //      price = this.price + " BYN.";
+ //      weight = this.weight + " г.";
+ //      quantity = this.quantity + " штук";
+ //      console.log(title, price, weight, quantity )
+ //    };
+ // };
+ //
+ //
+ // window.onload = function() {
+ //
+ //         document.getElementById("s1price").innerHTML = sm.price + " BYN.";
+ //         document.getElementById("s1title").innerHTML = sm.title;
+ //
+ // }
+
+  var sm = {
+    title : "СЯКИ МАКИ",
+    price : 2.5,
+    weight : 41,
+    quantity : 8
+  };
 
 
- var sm = {
-   title : "СЯКИ МАКИ",
-   price : 2.5,
-   weight : 41,
-   quantity : 8,
-   printOut : function () {
-      title = this.title;
-      price = this.price + " BYN.";
-      weight = this.weight + " г.";
-      quantity = this.quantity + " штук";
-      console.log(title, price, weight, quantity )
-    };
- };
 
- var um = {
-   title : "УНАГИ МАКИ",
-   price : 1.5,
-   weight : 42,
-   quantity : 8,
-   printOut : function () {
-      title = this.title;
-      price = this.price + " BYN.";
-      weight = this.weight + " г.";
-      quantity = this.quantity + " штук";
-      console.log(title, price, weight, quantity )
-    };
- };
+  window.onload = function() {
 
- var km = {
-   title : "КАППА МАКИ",
-   price : 0.5,
-   weight : 41,
-   quantity : 8,
-   printOut : function () {
-      title = this.title;
-      price = this.price + " BYN.";
-      weight = this.weight + " г.";
-      quantity = this.quantity + " штук";
-      console.log(title, price, weight, quantity )
-    };
- };
+          document.getElementById("s1price").innerHTML = sm.price + " BYN.";
+          document.getElementById("s1title").innerHTML = sm.title;
 
- var ssm = {
-   title : "СПАЙСИ СЯКЕ МАКИ",
-   price : 2.5,
-   weight : 41,
-   quantity : 8,
-   printOut : function () {
-      title = this.title;
-      price = this.price + " BYN.";
-      weight = this.weight + " г.";
-      quantity = this.quantity + " штук";
-      console.log(title, price, weight, quantity )
-    };
- };
-
-
- window.onload = function() {
-
-         document.getElementById("s1price").innerHTML = sm.price + " BYN.";
-         document.getElementById("s1title").innerHTML = sm.title;
-
- }
+  }
